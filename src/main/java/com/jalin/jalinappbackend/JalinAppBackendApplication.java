25fc7272771b16df152e342de53a317158c532f3
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-04 08:46:44 +0700

    Initial commit

---
+++ src/main/java/com/jalin/jalinappbackend/JalinAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jalin.jalinappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JalinAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JalinAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jalin/jalinappbackend/JalinAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jalin.jalinappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JalinAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
