de67530505eaa3256519a6b0d2c6be2d7ec2e9b6
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-07 16:24:14 +0700

    Add user and user details repositories

---
+++ src/main/java/com/jalin/jalinappbackend/module/authentication/repository/UserDetailsRepository.java
@@ -0,0 +1,13 @@
+package com.jalin.jalinappbackend.module.authentication.repository;
+
+import com.jalin.jalinappbackend.module.authentication.entity.UserDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserDetailsRepository extends JpaRepository<UserDetails, UUID> {
+    Optional<UserDetails> findByMobileNumber(String mobileNumber);
+}
