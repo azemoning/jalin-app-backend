b003660baf5dbe1073166675f4a36b45cd6675f6
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-08 23:46:30 +0700

    Add login service

--- src/main/java/com/jalin/jalinappbackend/module/authentication/service/AuthenticationService.java
+++ src/main/java/com/jalin/jalinappbackend/module/authentication/service/AuthenticationService.java
@@ -4,4 +4,5 @@ import com.jalin.jalinappbackend.module.authentication.entity.User;
 
 public interface AuthenticationService {
     void register(String mobileNumberRequestBody, User userRequestBody);
+    void login(String email, String password);
 }
--- src/main/java/com/jalin/jalinappbackend/module/authentication/service/AuthenticationServiceImpl.java
+++ src/main/java/com/jalin/jalinappbackend/module/authentication/service/AuthenticationServiceImpl.java
@@ -1,14 +1,24 @@
 package com.jalin.jalinappbackend.module.authentication.service;
 
+import com.jalin.jalinappbackend.exception.AuthenticateFailedException;
 import com.jalin.jalinappbackend.exception.RegisterFailedException;
+import com.jalin.jalinappbackend.exception.ResourceNotFoundException;
+import com.jalin.jalinappbackend.module.authentication.entity.Role;
+import com.jalin.jalinappbackend.module.authentication.entity.RoleEnum;
 import com.jalin.jalinappbackend.module.authentication.entity.User;
 import com.jalin.jalinappbackend.module.authentication.entity.UserDetails;
+import com.jalin.jalinappbackend.module.authentication.repository.RoleRepository;
 import com.jalin.jalinappbackend.module.authentication.repository.UserDetailsRepository;
 import com.jalin.jalinappbackend.module.authentication.repository.UserRepository;
 import com.jalin.jalinappbackend.module.authentication.service.model.FindCustomerByMobileNumberResponse;
 import com.jalin.jalinappbackend.utility.RestTemplateUtility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
@@ -18,9 +28,13 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     private static final String BASE_URL = "https://jalin-bank-resource-server.herokuapp.com";
     private static final String FIND_CUSTOMER_BY_MOBILE_PHONE_ENDPOINT = "/api/v1/customers/find?mobileNumber=";
     @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
     private RestTemplateUtility restTemplateUtility;
     @Autowired
     private PasswordEncoder passwordEncoder;
+    @Autowired
+    private RoleRepository roleRepository;
     @Autowired
     private UserRepository userRepository;
     @Autowired
@@ -38,9 +52,13 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                             BASE_URL + FIND_CUSTOMER_BY_MOBILE_PHONE_ENDPOINT + mobileNumberRequestBody,
                             FindCustomerByMobileNumberResponse.class);
 
+            Role userRole = roleRepository.findByRoleName(RoleEnum.USER)
+                    .orElseThrow(() -> new ResourceNotFoundException("Role not found"));
+
             User user = userRepository.save(new User(
                     userRequestBody.getEmail(),
-                    passwordEncoder.encode(userRequestBody.getPassword())));
+                    passwordEncoder.encode(userRequestBody.getPassword()),
+                    userRole));
 
             UserDetails userDetails = new UserDetails();
             userDetails.setFullName(response.getBody().getFullName());
@@ -50,6 +68,17 @@ public class AuthenticationServiceImpl implements AuthenticationService {
             userDetailsRepository.save(userDetails);
         } catch (HttpClientErrorException exception) {
             throw new RegisterFailedException("Mobile number not registered as Jalin Bank customer");
+        }
+    }
+
+    @Override
+    public void login(String email, String password) {
+        try {
+            Authentication authentication = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(email, password));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch (BadCredentialsException exception) {
+            throw new AuthenticateFailedException("Incorrect email or password");
         }
     }
 }
