c9cfa41e35e572b14b58ff8b86583d0573d540bf
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-08 23:48:16 +0700

    Add login endpoint

--- src/main/java/com/jalin/jalinappbackend/module/authentication/presenter/controller/AuthenticationController.java
+++ src/main/java/com/jalin/jalinappbackend/module/authentication/presenter/controller/AuthenticationController.java
@@ -2,21 +2,25 @@ package com.jalin.jalinappbackend.module.authentication.presenter.controller;
 
 import com.jalin.jalinappbackend.model.SuccessResponse;
 import com.jalin.jalinappbackend.module.authentication.entity.User;
+import com.jalin.jalinappbackend.module.authentication.entity.UserDetailsImpl;
+import com.jalin.jalinappbackend.module.authentication.jwt.JwtTokenUtility;
+import com.jalin.jalinappbackend.module.authentication.presenter.model.LoginRequest;
 import com.jalin.jalinappbackend.module.authentication.presenter.model.RegisterRequest;
 import com.jalin.jalinappbackend.module.authentication.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api")
 public class AuthenticationController {
+    @Autowired
+    private JwtTokenUtility jwtTokenUtility;
     @Autowired
     private AuthenticationService authenticationService;
 
@@ -28,5 +32,14 @@ public class AuthenticationController {
         return new ResponseEntity<>(
                 new SuccessResponse(true, "User registered successfully"),
                 HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Object> login(@Valid @RequestBody LoginRequest requestBody) {
+        authenticationService.login(requestBody.getEmail(), requestBody.getPassword());
+        UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return ResponseEntity.ok()
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtTokenUtility.generateToken(userDetails))
+                .body(new SuccessResponse(true, "Login successful"));
     }
 }
