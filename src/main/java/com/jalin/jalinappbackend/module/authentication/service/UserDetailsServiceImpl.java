94a791e1428a9d6e2a662e183be5d60dc278302f
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-10 03:34:31 +0700

    Change username not found exception to resource not found exception

--- src/main/java/com/jalin/jalinappbackend/module/authentication/service/UserDetailsServiceImpl.java
+++ src/main/java/com/jalin/jalinappbackend/module/authentication/service/UserDetailsServiceImpl.java
@@ -1,12 +1,12 @@
 package com.jalin.jalinappbackend.module.authentication.service;
 
+import com.jalin.jalinappbackend.exception.ResourceNotFoundException;
 import com.jalin.jalinappbackend.module.authentication.entity.User;
 import com.jalin.jalinappbackend.module.authentication.entity.UserDetailsImpl;
 import com.jalin.jalinappbackend.module.authentication.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,9 +15,9 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String email) {
         User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("Email not found"));
+                .orElseThrow(() -> new ResourceNotFoundException("Email not found"));
         return new UserDetailsImpl(user);
     }
 }
