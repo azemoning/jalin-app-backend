4f647ef866d5b055c9294e186b0f9968db351d66
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-12 15:39:44 +0700

    Add fund transfer endpoint

--- src/main/java/com/jalin/jalinappbackend/module/banking/presenter/controller/BankingController.java
+++ src/main/java/com/jalin/jalinappbackend/module/banking/presenter/controller/BankingController.java
@@ -1,13 +1,14 @@
 package com.jalin.jalinappbackend.module.banking.presenter.controller;
 
+import com.jalin.jalinappbackend.model.SuccessResponse;
 import com.jalin.jalinappbackend.module.banking.service.BankingService;
+import com.jalin.jalinappbackend.module.banking.service.model.FundTransferRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.math.BigDecimal;
 
 @RestController
@@ -20,5 +21,13 @@ public class BankingController {
     public ResponseEntity<Object> getAccountBalance() {
         BigDecimal accountBalance = bankingService.getAccountBalance();
         return new ResponseEntity<>(accountBalance, HttpStatus.OK);
+    }
+
+    @PostMapping("/transfers")
+    public ResponseEntity<Object> fundTransfer(@Valid @RequestBody FundTransferRequest requestBody) {
+        bankingService.fundTransfer(requestBody.getBeneficiaryAccountNumber(), requestBody.getAmount());
+        return new ResponseEntity<>(
+                new SuccessResponse(true, "Fund successfully transferred"),
+                HttpStatus.CREATED);
     }
 }
