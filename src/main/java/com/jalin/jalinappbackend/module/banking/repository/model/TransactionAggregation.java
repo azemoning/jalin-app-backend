0414c7116cddefc83eeca9c7ef3e7b947d81bb07
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-13 00:48:55 +0700

    Add transaction aggregation model

---
+++ src/main/java/com/jalin/jalinappbackend/module/banking/repository/model/TransactionAggregation.java
@@ -0,0 +1,17 @@
+package com.jalin.jalinappbackend.module.banking.repository.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class TransactionAggregation {
+    private String transactionName;
+    private String corporateNumber;
+    private String accountNumber;
+    private Long count;
+}
