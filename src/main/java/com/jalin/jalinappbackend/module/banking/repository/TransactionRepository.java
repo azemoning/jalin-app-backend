04c8d0e1b3a66b638db620deb8d71ffc891409d2
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-13 00:47:45 +0700

    Add find by user and find most frequent transactions query

--- src/main/java/com/jalin/jalinappbackend/module/banking/repository/TransactionRepository.java
+++ src/main/java/com/jalin/jalinappbackend/module/banking/repository/TransactionRepository.java
@@ -1,9 +1,23 @@
 package com.jalin.jalinappbackend.module.banking.repository;
 
+import com.jalin.jalinappbackend.module.authentication.entity.User;
 import com.jalin.jalinappbackend.module.banking.entity.Transaction;
+import com.jalin.jalinappbackend.module.banking.repository.model.TransactionAggregation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, String> {
+    List<Transaction> findByUser(User user);
+    @Query("SELECT new com.jalin.jalinappbackend.module.banking.repository.model.TransactionAggregation(" +
+            "t.transactionName, t.corporateNumber, t.accountNumber, COUNT(*)) " +
+            "FROM Transaction t " +
+            "WHERE t.user = :user " +
+            "AND t.transactionType = 'C' " +
+            "GROUP BY t.transactionName, t.corporateNumber, t.accountNumber " +
+            "ORDER BY COUNT(*) DESC")
+    List<TransactionAggregation> findMostFrequentTransactions(User user);
 }
