c38d271d804b4c925c9b2bf82c662a7b00821869
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-15 15:44:57 +0700

    Add check-in failed exception and its handler

---
+++ src/main/java/com/jalin/jalinappbackend/exception/CheckInFailedException.java
@@ -0,0 +1,7 @@
+package com.jalin.jalinappbackend.exception;
+
+public class CheckInFailedException extends RuntimeException {
+    public CheckInFailedException(String message) {
+        super(message);
+    }
+}
