d324b261d2f47522c9306db54ceade82ba3f97dc
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-15 12:33:39 +0700

    Add check-in log repository

---
+++ src/main/java/com/jalin/jalinappbackend/module/gamification/checkin/repository/CheckInLogRepository.java
@@ -0,0 +1,14 @@
+package com.jalin.jalinappbackend.module.gamification.checkin.repository;
+
+import com.jalin.jalinappbackend.module.authentication.entity.User;
+import com.jalin.jalinappbackend.module.gamification.checkin.entity.CheckInLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface CheckInLogRepository extends JpaRepository<CheckInLog, UUID> {
+    Optional<CheckInLog> findByUser(User user);
+}
