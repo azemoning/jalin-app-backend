6f0e7b7cf90db7d0036dddfaa3846f288bd83956
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-15 16:46:18 +0700

    Add date time format

--- src/main/java/com/jalin/jalinappbackend/module/authentication/presenter/model/RegisterRequest.java
+++ src/main/java/com/jalin/jalinappbackend/module/authentication/presenter/model/RegisterRequest.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -29,6 +30,7 @@ public class RegisterRequest {
     @NotBlank
     private String fullName;
     @NotNull
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate dateOfBirth;
     @NotBlank
     private String address;
