3fbf051e0b40787dbe26c91c1544dc8ec27ae563
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-15 16:33:17 +0700

    Change raw body request to form url encoded

--- src/main/java/com/jalin/jalinappbackend/module/authentication/presenter/controller/AuthenticationController.java
+++ src/main/java/com/jalin/jalinappbackend/module/authentication/presenter/controller/AuthenticationController.java
@@ -12,10 +12,11 @@ import com.jalin.jalinappbackend.utility.ModelMapperUtility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,20 +32,24 @@ public class AuthenticationController {
     @Autowired
     private AuthenticationService authenticationService;
 
-    @PostMapping("/register")
-    public ResponseEntity<Object> register(@Valid @RequestBody RegisterRequest requestBody) {
+    @PostMapping(
+            path = "/register",
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<Object> register(@Valid @ModelAttribute RegisterRequest requestBody) {
         User newUser = modelMapperUtility.initialize()
                 .map(requestBody, User.class);
         UserDetails newUserDetails = modelMapperUtility.initialize()
                 .map(requestBody, UserDetails.class);
         authenticationService.register(newUser, newUserDetails);
         return new ResponseEntity<>(
-                new SuccessResponse(true, "User successfully registered "),
+                new SuccessResponse(true, "User successfully registered"),
                 HttpStatus.CREATED);
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<Object> login(@Valid @RequestBody LoginRequest requestBody) {
+    @PostMapping(
+            path = "/login",
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<Object> login(@Valid @ModelAttribute LoginRequest requestBody) {
         authenticationService.login(requestBody.getEmail(), requestBody.getPassword());
         UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return ResponseEntity.ok()
