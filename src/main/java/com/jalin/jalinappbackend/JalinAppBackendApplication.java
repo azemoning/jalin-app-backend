b46c58dc8bdf24a44a92c96333b2731c5fc72b93
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-15 15:43:41 +0700

    Enable scheduling

--- src/main/java/com/jalin/jalinappbackend/JalinAppBackendApplication.java
+++ src/main/java/com/jalin/jalinappbackend/JalinAppBackendApplication.java
@@ -2,8 +2,10 @@ package com.jalin.jalinappbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class JalinAppBackendApplication {
 
 	public static void main(String[] args) {
