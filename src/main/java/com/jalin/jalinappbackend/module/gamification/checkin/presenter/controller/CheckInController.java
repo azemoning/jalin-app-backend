c49b768a518f1c6fda9f9f33ca4284368cf0e4ea
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-16 02:08:59 +0700

    Add get check-in status endpoint

--- src/main/java/com/jalin/jalinappbackend/module/gamification/checkin/presenter/controller/CheckInController.java
+++ src/main/java/com/jalin/jalinappbackend/module/gamification/checkin/presenter/controller/CheckInController.java
@@ -1,10 +1,12 @@
 package com.jalin.jalinappbackend.module.gamification.checkin.presenter.controller;
 
 import com.jalin.jalinappbackend.model.SuccessResponse;
+import com.jalin.jalinappbackend.module.gamification.checkin.model.CheckInDto;
 import com.jalin.jalinappbackend.module.gamification.checkin.service.CheckInService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class CheckInController {
     @Autowired
     private CheckInService checkInService;
+
+    @GetMapping("/check-in")
+    public ResponseEntity<Object> getCheckInStatus() {
+        CheckInDto checkInStatus = checkInService.getCheckInStatus();
+        return new ResponseEntity<>(checkInStatus, HttpStatus.OK);
+    }
 
     @PostMapping("/check-in")
     public ResponseEntity<Object> checkIn() {
