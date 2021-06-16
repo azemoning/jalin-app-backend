9b5c69c9bb5c60def1261fa6b1a2fd00745a9c60
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-16 22:08:50 +0700

    Remove unused import

--- src/main/java/com/jalin/jalinappbackend/configuration/SecurityConfiguration.java
+++ src/main/java/com/jalin/jalinappbackend/configuration/SecurityConfiguration.java
@@ -17,10 +17,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
@@ -44,7 +41,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                     };
                     c.configurationSource(source);
                 })
-        .csrf().disable();
+                .csrf().disable();
 
         http = http
                 .sessionManagement()
