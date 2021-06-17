e1e0234e73ca9ae5d496a28757fe977940bc24bf
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-17 09:58:47 +0700

    Add order annotation and try catch exception

--- src/main/java/com/jalin/jalinappbackend/seeder/RoleSeeder.java
+++ src/main/java/com/jalin/jalinappbackend/seeder/RoleSeeder.java
@@ -3,18 +3,27 @@ package com.jalin.jalinappbackend.seeder;
 import com.jalin.jalinappbackend.module.authentication.entity.Role;
 import com.jalin.jalinappbackend.module.authentication.entity.RoleEnum;
 import com.jalin.jalinappbackend.module.authentication.repository.RoleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
+@Order(value = 1)
+@Slf4j
 public class RoleSeeder implements CommandLineRunner {
     @Autowired
     private RoleRepository roleRepository;
 
     @Override
     public void run(String... args) {
-        seed();
+        try {
+            seed();
+        } catch (Exception exception) {
+            log.error(exception.getMessage());
+        }
+
     }
 
     private void seed() {
