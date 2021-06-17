339ce0a7c549a4a430fdcc1e3141a388fedf80bc
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-17 19:41:53 +0700

    Add get user profile endpoint

---
+++ src/main/java/com/jalin/jalinappbackend/module/authentication/presenter/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.jalin.jalinappbackend.module.authentication.presenter.controller;
+
+import com.jalin.jalinappbackend.module.authentication.model.UserDetailsDto;
+import com.jalin.jalinappbackend.module.authentication.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/profiles")
+    public ResponseEntity<Object> getUserProfile() {
+        UserDetailsDto userDetailsDto = userService.getUserDetails();
+        return new ResponseEntity<>(userDetailsDto, HttpStatus.OK);
+    }
+}
