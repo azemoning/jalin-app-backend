4eba2049ab2de3d066e623e74e3d099eec31fb03
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-17 19:40:54 +0700

    Add find by user query

--- src/main/java/com/jalin/jalinappbackend/module/authentication/repository/UserDetailsRepository.java
+++ src/main/java/com/jalin/jalinappbackend/module/authentication/repository/UserDetailsRepository.java
@@ -1,5 +1,6 @@
 package com.jalin.jalinappbackend.module.authentication.repository;
 
+import com.jalin.jalinappbackend.module.authentication.entity.User;
 import com.jalin.jalinappbackend.module.authentication.entity.UserDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -12,4 +13,5 @@ public interface UserDetailsRepository extends JpaRepository<UserDetails, UUID> 
     Optional<UserDetails> findByMobileNumber(String mobileNumber);
     Optional<UserDetails> findByAccountNumber(String accountNumber);
     Optional<UserDetails> findByIdCardNumber(String idCardNumber);
+    Optional<UserDetails> findByUser(User user);
 }
