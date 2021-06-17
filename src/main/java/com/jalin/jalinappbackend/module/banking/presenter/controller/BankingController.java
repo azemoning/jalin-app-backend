9505b5cec899442d516d5f46ff5895e378dd94b1
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-17 19:40:20 +0700

    Change request body from json to form url encoded

--- src/main/java/com/jalin/jalinappbackend/module/banking/presenter/controller/BankingController.java
+++ src/main/java/com/jalin/jalinappbackend/module/banking/presenter/controller/BankingController.java
@@ -2,12 +2,13 @@ package com.jalin.jalinappbackend.module.banking.presenter.controller;
 
 import com.jalin.jalinappbackend.model.SuccessResponse;
 import com.jalin.jalinappbackend.module.banking.model.TransactionDto;
+import com.jalin.jalinappbackend.module.banking.presenter.model.FundTransferRequest;
 import com.jalin.jalinappbackend.module.banking.repository.model.TransactionAggregation;
 import com.jalin.jalinappbackend.module.banking.service.BankingService;
 import com.jalin.jalinappbackend.module.banking.service.TransactionService;
-import com.jalin.jalinappbackend.module.banking.service.model.FundTransferRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -41,8 +42,10 @@ public class BankingController {
         return new ResponseEntity<>(transactionDto, HttpStatus.OK);
     }
 
-    @PostMapping("/transfers")
-    public ResponseEntity<Object> fundTransfer(@Valid @RequestBody FundTransferRequest requestBody) {
+    @PostMapping(
+            path = "/transfers",
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<Object> fundTransfer(@Valid @ModelAttribute FundTransferRequest requestBody) {
         bankingService.fundTransfer(requestBody.getBeneficiaryAccountNumber(), requestBody.getAmount());
         return new ResponseEntity<>(
                 new SuccessResponse(true, "Fund successfully transferred"),
