d2ce3412c0370d6a94a925307833c9b3d3b88e4e
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-26 03:36:12 +0700

    Add top up endpoints

--- src/main/java/com/jalin/jalinappbackend/module/banking/presenter/controller/TopUpController.java
+++ src/main/java/com/jalin/jalinappbackend/module/banking/presenter/controller/TopUpController.java
@@ -1,9 +1,14 @@
 package com.jalin.jalinappbackend.module.banking.presenter.controller;
 
+import com.jalin.jalinappbackend.module.banking.model.ConfirmTransferDto;
 import com.jalin.jalinappbackend.module.banking.model.CorporateDto;
+import com.jalin.jalinappbackend.module.banking.model.TransactionDto;
 import com.jalin.jalinappbackend.module.banking.model.WalletListDto;
 import com.jalin.jalinappbackend.module.banking.presenter.model.AddWalletListRequest;
+import com.jalin.jalinappbackend.module.banking.presenter.model.ConfirmTransferRequest;
+import com.jalin.jalinappbackend.module.banking.presenter.model.FundTransferRequest;
 import com.jalin.jalinappbackend.module.banking.service.CorporateService;
+import com.jalin.jalinappbackend.module.banking.service.TopUpService;
 import com.jalin.jalinappbackend.module.banking.service.WalletListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,6 +25,8 @@ public class TopUpController {
     @Autowired
     private CorporateService corporateService;
     @Autowired
+    private TopUpService topUpService;
+    @Autowired
     private WalletListService walletListService;
 
     @GetMapping("/transfers/virtual")
@@ -29,11 +36,32 @@ public class TopUpController {
     }
 
     @PostMapping(
+            path = "/transfers/virtual",
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<Object> fundTransferVirtual(@Valid @ModelAttribute FundTransferRequest requestBody) {
+        TransactionDto transactionDto = topUpService.fundTransferVirtual(
+                requestBody.getCorporateId(),
+                requestBody.getBeneficiaryAccountNumber(),
+                requestBody.getAmount(),
+                requestBody.getTransactionNote());
+        return new ResponseEntity<>(transactionDto, HttpStatus.CREATED);
+    }
+
+    @PostMapping(
             path = "/transfers/virtual/add",
             consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public ResponseEntity<Object> addWalletList(@Valid @ModelAttribute AddWalletListRequest requestBody) {
         WalletListDto walletListDto = walletListService.addWalletList(requestBody.getCorporateId(), requestBody.getAccountNumber());
         return new ResponseEntity<>(walletListDto, HttpStatus.CREATED);
+    }
+
+    @PostMapping(
+            path = "/transfers/virtual/confirm",
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<Object> confirmTransferVirtual(@Valid @ModelAttribute ConfirmTransferRequest requestBody) {
+        ConfirmTransferDto confirmTransferDto = topUpService.confirmTransfer(
+                requestBody.getCorporateId(), requestBody.getAccountNumber(), requestBody.getAmount());
+        return new ResponseEntity<>(confirmTransferDto, HttpStatus.OK);
     }
 
     @GetMapping("/transfers/corporates/wallet")
