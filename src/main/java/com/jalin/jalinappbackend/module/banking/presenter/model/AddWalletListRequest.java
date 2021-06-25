22f1cbf2a170dcf9d76bd53d2e97be8a93b12de2
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-26 02:33:13 +0700

    Add wallet list request model

---
+++ src/main/java/com/jalin/jalinappbackend/module/banking/presenter/model/AddWalletListRequest.java
@@ -0,0 +1,19 @@
+package com.jalin.jalinappbackend.module.banking.presenter.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class AddWalletListRequest {
+    @NotBlank
+    private String corporateId;
+    @NotBlank
+    private String accountNumber;
+}
