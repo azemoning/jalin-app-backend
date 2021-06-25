beced5f8a2283c815d7082dbcf4ce939da5a05e5
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-26 02:02:18 +0700

    Add add wallet list failed exception and its handler

---
+++ src/main/java/com/jalin/jalinappbackend/exception/AddWalletListFailedException.java
@@ -0,0 +1,7 @@
+package com.jalin.jalinappbackend.exception;
+
+public class AddWalletListFailedException extends RuntimeException {
+    public AddWalletListFailedException(String message) {
+        super(message);
+    }
+}
