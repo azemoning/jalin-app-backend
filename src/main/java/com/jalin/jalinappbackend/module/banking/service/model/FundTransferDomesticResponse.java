f6c9fdd7fc6282128266bebda11b9e1efa99bdbd
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-26 01:02:21 +0700

    Refactor rest template fund transfer domestic response model

--- src/main/java/com/jalin/jalinappbackend/module/banking/service/model/FundTransferDomesticResponse.java
+++ src/main/java/com/jalin/jalinappbackend/module/banking/service/model/FundTransferDomesticResponse.java
@@ -1,13 +1,12 @@
 package com.jalin.jalinappbackend.module.banking.service.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.Map;
 
 @NoArgsConstructor
@@ -15,22 +14,11 @@ import java.util.Map;
 @Getter
 @Setter
 public class FundTransferDomesticResponse {
-    private String transactionId;
-    private String transactionDate;
-    private String transactionType;
-    private String currency;
-    private BigDecimal amount;
-    private String transactionName;
-    private String transactionDescription;
+    private TransactionResponse sourceTransaction;
 
     @JsonProperty("data")
     private void unpackData(Map<String, Object> data) {
-        this.transactionId = (String) data.get("transactionId");
-        this.transactionDate = (String) data.get("transactionDate");
-        this.transactionType = (String) data.get("transactionType");
-        this.currency = (String) data.get("currency");
-        this.amount = BigDecimal.valueOf((Integer) data.get("amount")).setScale(2, RoundingMode.UNNECESSARY);
-        this.transactionName = (String) data.get("transactionName");
-        this.transactionDescription = (String) data.get("transactionDescription");
+        ObjectMapper objectMapper = new ObjectMapper();
+        this.sourceTransaction = objectMapper.convertValue(data, TransactionResponse.class);
     }
 }
