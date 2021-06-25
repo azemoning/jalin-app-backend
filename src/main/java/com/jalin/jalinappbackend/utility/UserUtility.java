c0cd60dfa127d12a47b5885d5a21f89f0c2b5731
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-26 01:00:14 +0700

    Add user utility

---
+++ src/main/java/com/jalin/jalinappbackend/utility/UserUtility.java
@@ -0,0 +1,22 @@
+package com.jalin.jalinappbackend.utility;
+
+import com.jalin.jalinappbackend.exception.ResourceNotFoundException;
+import com.jalin.jalinappbackend.module.authentication.entity.User;
+import com.jalin.jalinappbackend.module.authentication.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserUtility {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User getSignedInUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getPrincipal().toString();
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+    }
+}
