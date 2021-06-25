c1f5f1508cd4011f725864ad7cb02828c4166441
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-26 02:34:58 +0700

    Add wallet list endpoints

---
+++ src/main/java/com/jalin/jalinappbackend/module/banking/presenter/controller/TopUpController.java
@@ -0,0 +1,44 @@
+package com.jalin.jalinappbackend.module.banking.presenter.controller;
+
+import com.jalin.jalinappbackend.module.banking.model.CorporateDto;
+import com.jalin.jalinappbackend.module.banking.model.WalletListDto;
+import com.jalin.jalinappbackend.module.banking.presenter.model.AddWalletListRequest;
+import com.jalin.jalinappbackend.module.banking.service.CorporateService;
+import com.jalin.jalinappbackend.module.banking.service.WalletListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/banking")
+public class TopUpController {
+    @Autowired
+    private CorporateService corporateService;
+    @Autowired
+    private WalletListService walletListService;
+
+    @GetMapping("/transfers/virtual")
+    public ResponseEntity<Object> getWalletList() {
+        List<WalletListDto> walletListDto = walletListService.getWalletList();
+        return new ResponseEntity<>(walletListDto, HttpStatus.OK);
+    }
+
+    @PostMapping(
+            path = "/transfers/virtual/add",
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<Object> addWalletList(@Valid @ModelAttribute AddWalletListRequest requestBody) {
+        WalletListDto walletListDto = walletListService.addWalletList(requestBody.getCorporateId(), requestBody.getAccountNumber());
+        return new ResponseEntity<>(walletListDto, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/transfers/corporates/wallet")
+    public ResponseEntity<Object> getDigitalWalletCorporates() {
+        List<CorporateDto> corporateDtoList = corporateService.getDigitalWalletCorporates();
+        return new ResponseEntity<>(corporateDtoList, HttpStatus.OK);
+    }
+}
