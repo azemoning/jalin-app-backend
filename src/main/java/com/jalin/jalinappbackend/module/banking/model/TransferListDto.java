4acbdd2181cf8a1dc7765d7c4adf23b6c02461bb
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-25 15:39:01 +0700

    Add transfer list dto

---
+++ src/main/java/com/jalin/jalinappbackend/module/banking/model/TransferListDto.java
@@ -0,0 +1,16 @@
+package com.jalin.jalinappbackend.module.banking.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class TransferListDto {
+    private String corporateId;
+    private String accountNumber;
+    private String fullName;
+}
