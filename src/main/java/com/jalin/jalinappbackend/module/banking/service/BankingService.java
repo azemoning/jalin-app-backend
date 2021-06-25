b6819099c6b8c48d8dd7cc7dd4e9586216b2dfda
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-26 01:02:57 +0700

    Add confirm transfer service

--- src/main/java/com/jalin/jalinappbackend/module/banking/service/BankingService.java
+++ src/main/java/com/jalin/jalinappbackend/module/banking/service/BankingService.java
@@ -1,11 +1,13 @@
 package com.jalin.jalinappbackend.module.banking.service;
 
+import com.jalin.jalinappbackend.module.banking.model.ConfirmTransferDto;
 import com.jalin.jalinappbackend.module.banking.model.TransactionDto;
 
 import java.math.BigDecimal;
 
 public interface BankingService {
     BigDecimal getAccountBalance();
+    ConfirmTransferDto confirmTransfer(String corporateId, String beneficiaryAccountNumber, BigDecimal amount);
     TransactionDto fundTransfer(String beneficiaryAccountNumber, BigDecimal amount, String transactionNote);
     TransactionDto fundTransferDomestic(String corporateId, String beneficiaryAccountNumber, BigDecimal amount, String transactionNote);
 }
--- src/main/java/com/jalin/jalinappbackend/module/banking/service/BankingServiceImpl.java
+++ src/main/java/com/jalin/jalinappbackend/module/banking/service/BankingServiceImpl.java
@@ -2,27 +2,28 @@ package com.jalin.jalinappbackend.module.banking.service;
 
 import com.jalin.jalinappbackend.exception.ResourceNotFoundException;
 import com.jalin.jalinappbackend.exception.TransferFailedException;
-import com.jalin.jalinappbackend.module.authentication.entity.User;
 import com.jalin.jalinappbackend.module.authentication.entity.UserDetails;
 import com.jalin.jalinappbackend.module.authentication.repository.UserDetailsRepository;
-import com.jalin.jalinappbackend.module.authentication.repository.UserRepository;
 import com.jalin.jalinappbackend.module.banking.entity.Transaction;
+import com.jalin.jalinappbackend.module.banking.entity.TransferList;
+import com.jalin.jalinappbackend.module.banking.model.ConfirmTransferDto;
 import com.jalin.jalinappbackend.module.banking.model.TransactionDto;
 import com.jalin.jalinappbackend.module.banking.repository.TransactionRepository;
+import com.jalin.jalinappbackend.module.banking.repository.TransferListRepository;
 import com.jalin.jalinappbackend.module.banking.service.model.*;
 import com.jalin.jalinappbackend.utility.ModelMapperUtility;
 import com.jalin.jalinappbackend.utility.RestTemplateUtility;
+import com.jalin.jalinappbackend.utility.UserUtility;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.ZoneId;
@@ -35,14 +36,18 @@ public class BankingServiceImpl implements BankingService {
     private static final String GET_BANK_ACCOUNT_ENDPOINT = "/api/v1/accounts/";
     private static final String FUND_TRANSFER_ENDPOINT = "/api/v1/transfers";
     private static final String FUND_TRANSFER_DOMESTIC_ENDPOINT = "/api/v1/transfers/domestic";
+    private static final BigDecimal IDR_TRANSFER_FEE = new BigDecimal("6500").setScale(2, RoundingMode.UNNECESSARY);
+    private static final BigDecimal IDR_NO_TRANSFER_FEE = new BigDecimal("0").setScale(2, RoundingMode.UNNECESSARY);
     @Autowired
     private ModelMapperUtility modelMapperUtility;
     @Autowired
     private RestTemplateUtility restTemplateUtility;
     @Autowired
-    private UserRepository userRepository;
+    private UserUtility userUtility;
     @Autowired
     private UserDetailsRepository userDetailsRepository;
+    @Autowired
+    private TransferListRepository transferListRepository;
     @Autowired
     private TransactionRepository transactionRepository;
     @Autowired
@@ -50,7 +55,9 @@ public class BankingServiceImpl implements BankingService {
 
     @Override
     public BigDecimal getAccountBalance() {
-        UserDetails userDetails = getSignedInUserDetails();
+        UserDetails userDetails = userDetailsRepository.findByUser(userUtility.getSignedInUser())
+                .orElseThrow(() -> new ResourceNotFoundException("User details not found"));
+
         ResponseEntity<GetBankAccountResponse> response = restTemplateUtility.initialize().getForEntity(
                 BASE_URL + GET_BANK_ACCOUNT_ENDPOINT + userDetails.getAccountNumber(),
                 GetBankAccountResponse.class);
@@ -58,8 +65,36 @@ public class BankingServiceImpl implements BankingService {
     }
 
     @Override
+    public ConfirmTransferDto confirmTransfer(String corporateId, String beneficiaryAccountNumber, BigDecimal amount) {
+        TransferList transferList = transferListRepository
+                .findByUserAndCorporateIdAndAccountNumber(
+                        userUtility.getSignedInUser(), corporateId, beneficiaryAccountNumber)
+                .orElseThrow(() -> new ResourceNotFoundException("Bank account not found in transfer list"));
+
+        if (!corporateId.equals("212")) {
+            return initializeConfirmTransferDto(
+                    corporateId,
+                    corporateService.getCorporateByCorporateId(corporateId).getCorporateName(),
+                    beneficiaryAccountNumber,
+                    transferList.getFullName(),
+                    amount,
+                    IDR_TRANSFER_FEE);
+        } else {
+            return initializeConfirmTransferDto(
+                    corporateId,
+                    corporateService.getCorporateByCorporateId(corporateId).getCorporateName(),
+                    beneficiaryAccountNumber,
+                    transferList.getFullName(),
+                    amount,
+                    IDR_NO_TRANSFER_FEE);
+        }
+    }
+
+    @Override
     public TransactionDto fundTransfer(String beneficiaryAccountNumber, BigDecimal amount, String transactionNote) {
-        UserDetails userDetails = getSignedInUserDetails();
+        UserDetails userDetails = userDetailsRepository.findByUser(userUtility.getSignedInUser())
+                .orElseThrow(() -> new ResourceNotFoundException("User details not found"));
+
         FundTransferRequest request = new FundTransferRequest();
         request.setSourceAccountNumber(userDetails.getAccountNumber());
         request.setBeneficiaryAccountNumber(beneficiaryAccountNumber);
@@ -85,7 +120,7 @@ public class BankingServiceImpl implements BankingService {
             Transaction sourceTransaction = modelMapperUtility.initialize()
                     .map(response.getBody().getSourceTransaction(), Transaction.class);
             sourceTransaction.setTransactionDate(LocalDate.parse(response.getBody().getSourceTransaction().getTransactionDate()));
-            sourceTransaction.setCorporateId(getCorporateNumber(response.getBody().getSourceTransaction().getTransactionDescription()));
+            sourceTransaction.setCorporateId(getCorporateId(response.getBody().getSourceTransaction().getTransactionDescription()));
             sourceTransaction.setAccountNumber(getAccountNumber(response.getBody().getSourceTransaction().getTransactionDescription()));
             sourceTransaction.setTransactionMessage(getTransactionMessage(response.getBody().getSourceTransaction().getTransactionDescription()));
             sourceTransaction.setTransactionNote(transactionNote);
@@ -94,7 +129,7 @@ public class BankingServiceImpl implements BankingService {
             Transaction beneficiaryTransaction = modelMapperUtility.initialize()
                     .map(response.getBody().getBeneficiaryTransaction(), Transaction.class);
             beneficiaryTransaction.setTransactionDate(LocalDate.parse(response.getBody().getBeneficiaryTransaction().getTransactionDate()));
-            beneficiaryTransaction.setCorporateId(getCorporateNumber(response.getBody().getBeneficiaryTransaction().getTransactionDescription()));
+            beneficiaryTransaction.setCorporateId(getCorporateId(response.getBody().getBeneficiaryTransaction().getTransactionDescription()));
             beneficiaryTransaction.setAccountNumber(getAccountNumber(response.getBody().getBeneficiaryTransaction().getTransactionDescription()));
             beneficiaryTransaction.setTransactionMessage(getTransactionMessage(response.getBody().getBeneficiaryTransaction().getTransactionDescription()));
             beneficiaryTransaction.setTransactionNote(transactionNote);
@@ -117,7 +152,9 @@ public class BankingServiceImpl implements BankingService {
 
     @Override
     public TransactionDto fundTransferDomestic(String corporateId, String beneficiaryAccountNumber, BigDecimal amount, String transactionNote) {
-        UserDetails userDetails = getSignedInUserDetails();
+        UserDetails userDetails = userDetailsRepository.findByUser(userUtility.getSignedInUser())
+                .orElseThrow(() -> new ResourceNotFoundException("User details not found"));
+
         FundTransferDomesticRequest request = new FundTransferDomesticRequest();
         request.setSourceAccountNumber(userDetails.getAccountNumber());
         request.setCorporateId(corporateId);
@@ -133,10 +170,10 @@ public class BankingServiceImpl implements BankingService {
 
             Transaction transaction = modelMapperUtility.initialize()
                     .map(response.getBody(), Transaction.class);
-            transaction.setTransactionDate(LocalDate.parse(Objects.requireNonNull(response.getBody()).getTransactionDate()));
-            transaction.setCorporateId(getCorporateNumber(response.getBody().getTransactionDescription()));
-            transaction.setAccountNumber(getAccountNumber(response.getBody().getTransactionDescription()));
-            transaction.setTransactionMessage(getTransactionMessage(response.getBody().getTransactionDescription()));
+            transaction.setTransactionDate(LocalDate.parse(Objects.requireNonNull(response.getBody()).getSourceTransaction().getTransactionDate()));
+            transaction.setCorporateId(getCorporateId(response.getBody().getSourceTransaction().getTransactionDescription()));
+            transaction.setAccountNumber(getAccountNumber(response.getBody().getSourceTransaction().getTransactionDescription()));
+            transaction.setTransactionMessage(getTransactionMessage(response.getBody().getSourceTransaction().getTransactionDescription()));
             transaction.setTransactionNote(transactionNote);
             transaction.setUser(userDetails.getUser());
 
@@ -153,16 +190,24 @@ public class BankingServiceImpl implements BankingService {
         }
     }
 
-    private UserDetails getSignedInUserDetails() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String email = authentication.getPrincipal().toString();
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
-        return userDetailsRepository.findById(user.getUserId())
-                .orElseThrow(() -> new ResourceNotFoundException("User details not found"));
+    private ConfirmTransferDto initializeConfirmTransferDto(
+            String corporateId,
+            String corporateName,
+            String accountNumber,
+            String fullName,
+            BigDecimal transferAmount,
+            BigDecimal transferFee) {
+        return new ConfirmTransferDto(
+                corporateId,
+                corporateName,
+                accountNumber,
+                fullName,
+                transferAmount,
+                transferFee,
+                transferAmount.add(transferFee));
     }
 
-    private String getCorporateNumber(String transactionDescription) {
+    private String getCorporateId(String transactionDescription) {
         String[] numbers = transactionDescription.split("/");
         return numbers[0];
     }
