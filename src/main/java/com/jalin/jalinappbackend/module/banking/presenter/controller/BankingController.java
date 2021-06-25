7986996e6b94b26927f87ad60b7c05b786e39e23
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-25 17:32:30 +0700

    Add endpoints for get transfer list and add transfer list. Merge endpoints fund transfer and fund transfer domestic into one endpoint

--- src/main/java/com/jalin/jalinappbackend/module/banking/presenter/controller/BankingController.java
+++ src/main/java/com/jalin/jalinappbackend/module/banking/presenter/controller/BankingController.java
@@ -1,14 +1,16 @@
 package com.jalin.jalinappbackend.module.banking.presenter.controller;
 
 import com.jalin.jalinappbackend.model.SuccessDetailsResponse;
-import com.jalin.jalinappbackend.model.SuccessResponse;
 import com.jalin.jalinappbackend.module.banking.model.CorporateDto;
 import com.jalin.jalinappbackend.module.banking.model.TransactionDto;
-import com.jalin.jalinappbackend.module.banking.presenter.model.FundTransferDomesticRequest;
+import com.jalin.jalinappbackend.module.banking.model.TransferListDto;
+import com.jalin.jalinappbackend.module.banking.presenter.model.AddTransferListRequest;
 import com.jalin.jalinappbackend.module.banking.presenter.model.FundTransferRequest;
 import com.jalin.jalinappbackend.module.banking.repository.model.TransactionAggregation;
 import com.jalin.jalinappbackend.module.banking.service.BankingService;
+import com.jalin.jalinappbackend.module.banking.service.CorporateService;
 import com.jalin.jalinappbackend.module.banking.service.TransactionService;
+import com.jalin.jalinappbackend.module.banking.service.TransferListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -25,6 +27,10 @@ import java.util.Set;
 public class BankingController {
     @Autowired
     private BankingService bankingService;
+    @Autowired
+    private CorporateService corporateService;
+    @Autowired
+    private TransferListService transferListService;
     @Autowired
     private TransactionService transactionService;
 
@@ -46,32 +52,43 @@ public class BankingController {
         return new ResponseEntity<>(transactionDto, HttpStatus.OK);
     }
 
+    @GetMapping("/transfers")
+    public ResponseEntity<Object> getTransferList() {
+        List<TransferListDto> transferListDto = transferListService.getTransferList();
+        return new ResponseEntity<>(transferListDto, HttpStatus.OK);
+    }
+
     @PostMapping(
             path = "/transfers",
             consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public ResponseEntity<Object> fundTransfer(@Valid @ModelAttribute FundTransferRequest requestBody) {
-        bankingService.fundTransfer(requestBody.getBeneficiaryAccountNumber(), requestBody.getAmount());
-        return new ResponseEntity<>(
-                new SuccessResponse(true, "Fund successfully transferred"),
-                HttpStatus.CREATED);
-    }
-
-    @PostMapping(
-            path = "/transfers/domestic",
-            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public ResponseEntity<Object> fundTransferDomestic(@Valid @ModelAttribute FundTransferDomesticRequest requestBody) {
-        TransactionDto transactionDto = bankingService.fundTransferDomestic(
-                requestBody.getCorporateId(),
-                requestBody.getBeneficiaryAccountNumber(),
-                requestBody.getAmount());
+        TransactionDto transactionDto;
+        if (!requestBody.getCorporateId().equals("212")) {
+            transactionDto = bankingService.fundTransferDomestic(
+                    requestBody.getCorporateId(),
+                    requestBody.getBeneficiaryAccountNumber(),
+                    requestBody.getAmount());
+        } else {
+            transactionDto = bankingService.fundTransfer(
+                    requestBody.getBeneficiaryAccountNumber(),
+                    requestBody.getAmount());
+        }
         return new ResponseEntity<>(
                 new SuccessDetailsResponse(true, "Fund successfully transferred", transactionDto),
                 HttpStatus.CREATED);
     }
 
+    @PostMapping(
+            path = "/transfers/add",
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<Object> addTransferList(@Valid @ModelAttribute AddTransferListRequest requestBody) {
+        TransferListDto transferListDto = transferListService.addTransferList(requestBody.getCorporateId(), requestBody.getAccountNumber());
+        return new ResponseEntity<>(transferListDto, HttpStatus.CREATED);
+    }
+
     @GetMapping("/transfers/corporates")
     public ResponseEntity<Object> getBankCorporates() {
-        List<CorporateDto> corporateDtoList = bankingService.getBankCorporates();
+        List<CorporateDto> corporateDtoList = corporateService.getBankCorporates();
         return new ResponseEntity<>(corporateDtoList, HttpStatus.OK);
     }
 }
