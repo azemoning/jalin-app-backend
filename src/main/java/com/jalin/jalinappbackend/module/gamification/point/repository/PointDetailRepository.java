80b291fc595590704058117602f21d81f9c77160
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-30 20:46:03 +0700

    Add find by point query

--- src/main/java/com/jalin/jalinappbackend/module/gamification/point/repository/PointDetailRepository.java
+++ src/main/java/com/jalin/jalinappbackend/module/gamification/point/repository/PointDetailRepository.java
@@ -1,11 +1,14 @@
 package com.jalin.jalinappbackend.module.gamification.point.repository;
 
+import com.jalin.jalinappbackend.module.gamification.point.entity.Point;
 import com.jalin.jalinappbackend.module.gamification.point.entity.PointDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface PointDetailRepository extends JpaRepository<PointDetail, UUID> {
+    List<PointDetail> findByPoint(Point point);
 }
