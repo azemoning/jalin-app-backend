72363df3dbe3c4ca39be18c8e60af581e73b96df
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-06-30 20:46:22 +0700

    Add get user point details endpoint

--- src/main/java/com/jalin/jalinappbackend/module/gamification/point/presenter/controller/PointController.java
+++ src/main/java/com/jalin/jalinappbackend/module/gamification/point/presenter/controller/PointController.java
@@ -1,5 +1,7 @@
 package com.jalin.jalinappbackend.module.gamification.point.presenter.controller;
 
+import com.jalin.jalinappbackend.module.gamification.point.model.PointDetailDto;
+import com.jalin.jalinappbackend.module.gamification.point.model.PointDto;
 import com.jalin.jalinappbackend.module.gamification.point.service.PointService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -7,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -17,6 +21,13 @@ public class PointController {
 
     @GetMapping("/points")
     public ResponseEntity<Object> getUserPoint() {
-        return new ResponseEntity<>(pointService.getUserPoint(), HttpStatus.OK);
+        PointDto pointDto = pointService.getUserPoint();
+        return new ResponseEntity<>(pointDto, HttpStatus.OK);
+    }
+
+    @GetMapping("/points/details")
+    public ResponseEntity<Object> getUserPointDetails() {
+        List<PointDetailDto> pointDetailDtoList = pointService.getUserPointDetails();
+        return new ResponseEntity<>(pointDetailDtoList, HttpStatus.OK);
     }
 }
