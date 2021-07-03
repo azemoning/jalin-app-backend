c7d960a404f63ba200d20126680fc698a1651161
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-07-03 19:14:41 +0700

    Add confirm payment request model

---
+++ src/main/java/com/jalin/jalinappbackend/module/banking/presenter/model/ConfirmPaymentRequest.java
@@ -0,0 +1,26 @@
+package com.jalin.jalinappbackend.module.banking.presenter.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ConfirmPaymentRequest {
+    @NotBlank
+    private String corporateId;
+    @Positive
+    private BigDecimal amount;
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount.setScale(2, RoundingMode.UNNECESSARY);
+    }
+}
