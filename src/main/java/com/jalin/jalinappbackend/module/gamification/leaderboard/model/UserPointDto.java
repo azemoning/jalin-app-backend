ed4e661796c5c5248564b1577e4b8d2672f895c8
ilyasmahfud <ilyasmahfud39@gmail.com>
2021-07-05 21:21:28 +0700

    delet model

--- src/main/java/com/jalin/jalinappbackend/module/gamification/leaderboard/model/UserPointDto.java
+++
@@ -1,9 +0,0 @@
-package com.jalin.jalinappbackend.module.gamification.leaderboard.model;
-
-import java.util.UUID;
-
-public class UserPointDto {
-    private UUID id;
-    private Integer point;
-}
-
