a3a36d8abbcadc8b7f5740639a1858428c8dec3d
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-07-07 21:49:24 +0700

    Add prepaid mobile phone dto

---
+++ src/main/java/com/jalin/jalinappbackend/module/banking/model/PrepaidMobilePhoneDto.java
@@ -0,0 +1,20 @@
+package com.jalin.jalinappbackend.module.banking.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class PrepaidMobilePhoneDto {
+    private String providerId;
+    private String productName;
+    private String providerName;
+    private String mobilePhoneNumber;
+    private List<PrepaidDto> prepaidList;
+}
