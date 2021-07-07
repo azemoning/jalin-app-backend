ee0f15fd2a16e87f41001c7c91584399292130fe
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-07-07 21:49:16 +0700

    Add rest template prepaid option model

---
+++ src/main/java/com/jalin/jalinappbackend/module/banking/service/model/payment/PrepaidOption.java
@@ -0,0 +1,35 @@
+package com.jalin.jalinappbackend.module.banking.service.model.payment;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Map;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class PrepaidOption {
+    private UUID prepaidId;
+    private String prepaidName;
+    private BigDecimal price;
+
+    public void setPrice(BigDecimal amount) {
+        this.price = amount.setScale(2, RoundingMode.UNNECESSARY);
+    }
+
+    @JsonProperty("data")
+    private void unpackData(Map<String, Object> data) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        this.prepaidId = objectMapper.convertValue(data.get("prepaidId"), UUID.class);
+        this.prepaidName = (String) data.get("prepaidName");
+        this.price = BigDecimal.valueOf((Double) data.get("price")).setScale(2, RoundingMode.UNNECESSARY);
+    }
+}
