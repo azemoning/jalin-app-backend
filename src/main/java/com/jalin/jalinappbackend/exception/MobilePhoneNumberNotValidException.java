8a4bad4270dbb5738749a25d5e654e7672b9de9f
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-07-07 21:47:37 +0700

    Add mobile phone number not valid exception and its handler

---
+++ src/main/java/com/jalin/jalinappbackend/exception/MobilePhoneNumberNotValidException.java
@@ -0,0 +1,7 @@
+package com.jalin.jalinappbackend.exception;
+
+public class MobilePhoneNumberNotValidException extends RuntimeException {
+    public MobilePhoneNumberNotValidException(String message) {
+        super(message);
+    }
+}
