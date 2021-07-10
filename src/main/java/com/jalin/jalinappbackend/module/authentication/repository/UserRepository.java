26bb29c5084ac7560e6a78d9b082f495bf00f2ed
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-07-10 19:35:15 +0700

    Add find by role query

--- src/main/java/com/jalin/jalinappbackend/module/authentication/repository/UserRepository.java
+++ src/main/java/com/jalin/jalinappbackend/module/authentication/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.jalin.jalinappbackend.module.authentication.repository;
 
+import com.jalin.jalinappbackend.module.authentication.entity.Role;
 import com.jalin.jalinappbackend.module.authentication.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +11,5 @@ import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
+    Optional<User> findByRole(Role role);
 }
