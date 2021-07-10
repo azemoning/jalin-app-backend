1be66a53f78a7f64f51ac471651bd8797fc9a417
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-07-10 20:57:32 +0700

    Add find user list by role query

--- src/main/java/com/jalin/jalinappbackend/module/authentication/repository/UserRepository.java
+++ src/main/java/com/jalin/jalinappbackend/module/authentication/repository/UserRepository.java
@@ -5,11 +5,12 @@ import com.jalin.jalinappbackend.module.authentication.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
-    Optional<User> findByRole(Role role);
+    List<User> findByRole(Role role);
 }
