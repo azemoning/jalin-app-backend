0534989e4042222fab6eb264014dc900e8de006b
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-07-10 19:34:13 +0700

    Add login admin service

--- src/main/java/com/jalin/jalinappbackend/module/authentication/service/AuthenticationService.java
+++ src/main/java/com/jalin/jalinappbackend/module/authentication/service/AuthenticationService.java
@@ -2,9 +2,11 @@ package com.jalin.jalinappbackend.module.authentication.service;
 
 import com.jalin.jalinappbackend.module.authentication.entity.User;
 import com.jalin.jalinappbackend.module.authentication.entity.UserDetails;
+import com.jalin.jalinappbackend.module.authentication.model.LoginAdminDto;
 import com.jalin.jalinappbackend.module.authentication.model.LoginDto;
 
 public interface AuthenticationService {
     void register(User userRequestBody, UserDetails userDetailsRequestBody);
     LoginDto login(String email, String password);
+    LoginAdminDto loginAdmin(String email, String password);
 }
