a9f8268a5e71dd28bef188e51f356b9499f48d47
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-07-10 20:59:03 +0700

    Remove unused imports

--- src/main/java/com/jalin/jalinappbackend/module/dashboard/service/ActiveCustomerServiceImpl.java
+++ src/main/java/com/jalin/jalinappbackend/module/dashboard/service/ActiveCustomerServiceImpl.java
@@ -1,16 +1,10 @@
 package com.jalin.jalinappbackend.module.dashboard.service;
 
-import com.jalin.jalinappbackend.exception.ResourceNotFoundException;
-import com.jalin.jalinappbackend.module.authentication.entity.Role;
-import com.jalin.jalinappbackend.module.authentication.entity.RoleEnum;
-import com.jalin.jalinappbackend.module.authentication.entity.User;
 import com.jalin.jalinappbackend.module.authentication.repository.RoleRepository;
 import com.jalin.jalinappbackend.module.authentication.repository.UserRepository;
 import com.jalin.jalinappbackend.utility.UserUtility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class ActiveCustomerServiceImpl implements ActiveCustomerService {
