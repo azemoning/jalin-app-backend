624d65f74f5e01ff2bfe9e203f31b9e97ef4e403
Kristian Sandratama <kristian.sandratama@outlook.com>
2021-07-10 19:35:50 +0700

    Add login admin dto

---
+++ src/main/java/com/jalin/jalinappbackend/module/authentication/model/LoginAdminDto.java
@@ -0,0 +1,16 @@
+package com.jalin.jalinappbackend.module.authentication.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class LoginAdminDto {
+    private String email;
+    private String role;
+    private String token;
+}
