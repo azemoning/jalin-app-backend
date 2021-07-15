536103ed0d44ffd0aa088545071c99833bd94839
azemoning <ahmadsaufimaulana@gmail.com>
2021-07-15 20:27:15 +0700

    update api response with additional url parameters

--- src/main/java/com/jalin/jalinappbackend/module/gamification/checkin/repository/CheckInRepository.java
+++ src/main/java/com/jalin/jalinappbackend/module/gamification/checkin/repository/CheckInRepository.java
@@ -5,10 +5,13 @@ import com.jalin.jalinappbackend.module.gamification.checkin.entity.CheckIn;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CheckInRepository extends JpaRepository<CheckIn, UUID> {
     Optional<CheckIn> findFirstByUserOrderByCreatedDateDesc(User user);
+    List<CheckIn> findCheckInByCreatedDateBetween(Instant startDate, Instant endDate);
 }
