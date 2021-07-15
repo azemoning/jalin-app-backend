68f1beebfdba33e7903745d5b418b2703ff3d1a5
Upi <ahmadsaufimaulana@gmail.com>
2021-07-15 13:38:56 +0000

    [hotfix] fix typo on api endpoint

--- src/main/java/com/jalin/jalinappbackend/module/dashboard/presenter/controller/LeaderboardDataController.java
+++ src/main/java/com/jalin/jalinappbackend/module/dashboard/presenter/controller/LeaderboardDataController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/admmin/v1")
+@RequestMapping("api/admin/v1")
 public class LeaderboardDataController {
     @Autowired
     LeaderboardDataService leaderboardDataService;
